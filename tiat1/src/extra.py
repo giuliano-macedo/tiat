240f864a3902900569d1e934b4432787f6032e63
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-03-22 11:07:10 -0400

    extra

--- tiat1/src/extra.py
+++ tiat1/src/extra.py
@@ -4,25 +4,30 @@ from args import get_args
 from tqdm import tqdm
 import pickle
 
-args=get_args(p=100,m=0.01)
+args=get_args(p=1000,m=0.01)
 
 np.random.seed(args.seed)
 
 graph=Graph("states.json","Mato Grosso do Sul")
 
+with open("best_extra.p","rb") as f:
+	initial=pickle.load(f)
 pool=Pool(
 	graph,
 	args.population_size,
 	args.crossover_rate,
-	args.mutation_rate
+	args.mutation_rate,
+	initial
 )
+no_iterations=int(5e4)
 
-times=[]
-no_iterations=int(1e6)
+last_best=None
 
 for i in tqdm(range(no_iterations)):
 	pool.step()
-	tqdm.write(f"gen {pool.gen:4} cost:{pool.best.cost:10}")
-with open("best_extra.p","wb") as f:
-	pickle.dump(pool.best.genes,f)
+	if last_best==None or last_best.cost!=pool.best.cost:
+		tqdm.write(f"gen {pool.gen:4} cost:{pool.best.cost:10}")
+		last_best=pool.best
+		with open("best_extra.p","wb") as f:
+			pickle.dump(pool.best.genes,f)
 pool.best.print(pretty=False)
